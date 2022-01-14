e47e2aee15b51b50d3c99056ee4c205a17c5e279
Victor <victor_martins_castro@hotmail.com>
2022-01-14 18:32:57 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,9 +29,15 @@ function Listing() {
             })
     }, [pageNumber])
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
+
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
@@ -43,8 +49,6 @@ function Listing() {
                         </div>
                     )
                     )}
-
-
 
                 </div>
             </div>
